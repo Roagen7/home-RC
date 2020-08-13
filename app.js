ffdcf66b6e1586b38969148cb8384372a62110cf
Roagen7 <harek2388@gmail.com>
2020-08-13 09:32:56 +0200

    sending messages to file

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 "use strict";
 exports.__esModule = true;
 var http_1 = require("http");
+//import { query } from 'mysql';
+var fs_1 = require("fs");
 var url_1 = require("url");
-var host = "localhost";
+var host = 'localhost';
 var port = 3000;
 http_1.createServer(function (req, res) {
     var q = url_1.parse(req.url, true);
-    res.writeHead(200, { "Content-type": "text/html" });
-    if (q.pathname == "/message") {
-        if (q.query["user"] && q.query["message"]) {
-            console.log(q.query);
-            res.write(">>" + q.query.user + ":" + q.query.message);
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    if (q.pathname == '/message') {
+        if (q.query['user'] && q.query['message']) {
+            fs_1.appendFile('messages.txt', "\n[" + q.query.user + "]>>" + q.query.message, function (err) {
+                if (err)
+                    throw err;
+                console.log('saved!');
+            });
+            res.end("message sent <button onclick=\"myRedirect()\">go back</button> <script> function myRedirect(){\n        window.location.replace('http://" + host + ":" + port + "/');\n      }</script>");
         }
     }
-    if (q.pathname == "/") {
+    if (q.pathname == '/') {
         res.write("\n            <form action=\"/message\" method=\"get\">\n                <label for=\"user\">username: <input id=\"user\" name=\"user\" type=\"text\"> </input> </label>\n                <label for=\"message\">message: <input id=\"message\" name=\"message\" type=\"text\"> </input> </label>\n                <label for=\"sender\"><input type=\"submit\"></input></label>\n            </form>\n        ");
     }
     res.end();
-}).listen(port, host, function () {
-    return console.log("server listening at http://" + host + ":" + port);
-});
+}).listen(port, host, function () { return console.log("server listening at http://" + host + ":" + port); });
--- app.ts
+++ app.ts
@@ -1,25 +1,28 @@
-import {
-  createServer,
-  IncomingHttpHeaders,
-  IncomingMessage,
-  ServerResponse,
-} from "http";
-import { query } from "mysql";
-import { parse } from "url";
+import { createServer, IncomingHttpHeaders, IncomingMessage, ServerResponse } from 'http';
+//import { query } from 'mysql';
+import { appendFile } from 'fs';
+import { parse } from 'url';
 
-const host: string = "localhost";
+const host: string = 'localhost';
 const port: number = 3000;
 
 createServer((req: IncomingMessage, res: ServerResponse) => {
   const q: any = parse(req.url, true);
-  res.writeHead(200, { "Content-type": "text/html" });
-  if (q.pathname == "/message") {
-    if (q.query["user"] && q.query["message"]) {
-      res.write(`[${q.query.user}]>>${q.query.message}`);
+  res.writeHead(200, { 'Content-type': 'text/html' });
+  if (q.pathname == '/message') {
+    if (q.query['user'] && q.query['message']) {
+      appendFile('messages.txt', `\n[${q.query.user}]>>${q.query.message}`, (err) => {
+        if (err) throw err;
+        console.log('saved!');
+      });
+
+      res.end(`message sent <button onclick="myRedirect()">go back</button> <script> function myRedirect(){
+        window.location.replace('http://${host}:${port}/');
+      }</script>`);
     }
   }
 
-  if (q.pathname == "/") {
+  if (q.pathname == '/') {
     res.write(`
             <form action="/message" method="get">
                 <label for="user">username: <input id="user" name="user" type="text"> </input> </label>
@@ -30,6 +33,4 @@ createServer((req: IncomingMessage, res: ServerResponse) => {
   }
 
   res.end();
-}).listen(port, host, () =>
-  console.log(`server listening at http://${host}:${port}`)
-);
+}).listen(port, host, () => console.log(`server listening at http://${host}:${port}`));
