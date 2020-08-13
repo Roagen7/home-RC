7707327ea8d29195f72c935f4a561d406256b2ee
Roagen7 <harek2388@gmail.com>
2020-08-13 11:43:10 +0200

    dodanie obsługi post

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 "use strict";
+//import { query } from 'mysql';
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 exports.__esModule = true;
 var express_1 = __importDefault(require("express"));
+var body_parser_1 = __importDefault(require("body-parser"));
 var host = 'localhost';
 var port = 3000;
 var app = express_1["default"]();
+var router = express_1["default"].Router();
+app.use(body_parser_1["default"].urlencoded({ extended: false }));
+app.use(body_parser_1["default"].json());
+router.post('/', function (req, res) {
+    console.log(req.body);
+});
+app.use('/message', router);
 app.get('/', function (req, res) {
-    res.send('helllo');
+    res.send(" <form action=\"/message\" method=\"POST\">\n  <label for=\"user\">username: <input id=\"user\" name=\"user\" type=\"text\"> </input> </label>\n  <label for=\"message\">message: <input id=\"message\" name=\"message\" type=\"text\"> </input> </label>\n  <label for=\"sender\"><input type=\"submit\"></input></label>\n</form>");
 });
 app.listen(port, function () {
     console.log("listening at http://" + host + ":" + port);
--- app.ts
+++ app.ts
@@ -1,16 +1,28 @@
-import { createServer, IncomingHttpHeaders, IncomingMessage, ServerResponse } from 'http';
 //import { query } from 'mysql';
-import { appendFile, readFile, exists, read, fstat } from 'fs';
-import { parse } from 'url';
+
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const host: string = 'localhost';
 const port: number = 3000;
 
 const app = express();
+const router = express.Router();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+router.post('/', (req, res) => {});
+
+app.use('/message', router);
+
 app.get('/', (req, res) => {
-  res.send('helllo');
+  res.send(` <form action="/message" method="POST">
+  <label for="user">username: <input id="user" name="user" type="text"> </input> </label>
+  <label for="message">message: <input id="message" name="message" type="text"> </input> </label>
+  <label for="sender"><input type="submit"></input></label>
+</form>`);
 });
+
 app.listen(port, () => {
   console.log(`listening at http://${host}:${port}`);
 });
