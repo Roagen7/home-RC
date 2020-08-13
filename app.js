41808ee0b15d51780a1e7a629ef07d9fab94ed59
Roagen7 <harek2388@gmail.com>
2020-08-13 12:30:58 +0200

    database works + chat dashboard

--- app.js
+++ app.js
@@ -6,18 +6,53 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 exports.__esModule = true;
 var express_1 = __importDefault(require("express"));
 var body_parser_1 = __importDefault(require("body-parser"));
+var mysql_1 = __importDefault(require("mysql"));
 var host = 'localhost';
 var port = 3000;
+var con = mysql_1["default"].createConnection({
+    host: host,
+    user: 'root',
+    password: '',
+    database: 'messages'
+});
+/*
+con.connect((err) => {
+  if (err) throw err;
+  
+  con.query('CREATE DATABASE messages', (err, result) => {
+    if (err) throw err;
+    console.log('created database');
+  });
+ 
+  con.query('CREATE TABLE msg (nick VARCHAR(255), text VARCHAR(255))', (err, result) => {
+    if (err) throw err;
+    console.log('created table');
+  });
+  
+});
+*/
 var app = express_1["default"]();
 var router = express_1["default"].Router();
 app.use(body_parser_1["default"].urlencoded({ extended: false }));
 app.use(body_parser_1["default"].json());
 router.post('/', function (req, res) {
-    console.log(req.body);
+    if (req.body) {
+        console.log(req.body);
+        con.query("INSERT INTO msg (nick, text) VALUES (\"" + req.body.user + "\", \"" + req.body.message + "\")");
+    }
+    res.redirect('/');
 });
 app.use('/message', router);
 app.get('/', function (req, res) {
-    res.send(" <form action=\"/message\" method=\"POST\">\n  <label for=\"user\">username: <input id=\"user\" name=\"user\" type=\"text\"> </input> </label>\n  <label for=\"message\">message: <input id=\"message\" name=\"message\" type=\"text\"> </input> </label>\n  <label for=\"sender\"><input type=\"submit\"></input></label>\n</form>");
+    con.query('SELECT nick, text FROM msg', function (err, result) {
+        var returnedMessage = '<div><ul>';
+        for (var _i = 0, result_1 = result; _i < result_1.length; _i++) {
+            var message = result_1[_i];
+            returnedMessage += "<span>[<b>" + message.nick + "</b>] >> " + message.text + "</span><br>";
+        }
+        returnedMessage += " </ul></div><form action=\"/message\" method=\"POST\">\n  <label for=\"user\">username: <input id=\"user\" name=\"user\" type=\"text\"> </input> </label>\n  <label for=\"message\">message: <input id=\"message\" name=\"message\" type=\"text\"> </input> </label>\n  <label for=\"sender\"><input type=\"submit\"></input></label>\n  </form>";
+        res.send(returnedMessage);
+    });
 });
 app.listen(port, function () {
     console.log("listening at http://" + host + ":" + port);
--- app.ts
+++ app.ts
@@ -1,26 +1,64 @@
 //import { query } from 'mysql';
 
-import express from 'express';
+import express, { Router } from 'express';
 import bodyParser from 'body-parser';
+import mysql from 'mysql';
 
 const host: string = 'localhost';
 const port: number = 3000;
 
+const con = mysql.createConnection({
+  host,
+  user: 'root',
+  password: '',
+  database: 'messages'
+});
+/*
+con.connect((err) => {
+  if (err) throw err;
+  
+  con.query('CREATE DATABASE messages', (err, result) => {
+    if (err) throw err;
+    console.log('created database');
+  });
+ 
+  con.query('CREATE TABLE msg (nick VARCHAR(255), text VARCHAR(255))', (err, result) => {
+    if (err) throw err;
+    console.log('created table');
+  });
+  
+});
+*/
+
 const app = express();
-const router = express.Router();
+const router: Router = express.Router();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-router.post('/', (req, res) => {});
+router.post('/', (req, res) => {
+  if (req.body) {
+    console.log(req.body);
+    con.query(`INSERT INTO msg (nick, text) VALUES ("${req.body.user}", "${req.body.message}")`);
+  }
+  res.redirect('/');
+});
 
 app.use('/message', router);
 
 app.get('/', (req, res) => {
-  res.send(` <form action="/message" method="POST">
+  con.query('SELECT nick, text FROM msg', (err, result) => {
+    let returnedMessage = '<div><ul>';
+
+    for (const message of result) {
+      returnedMessage += `<span>[<b>${message.nick}</b>] >> ${message.text}</span><br>`;
+    }
+    returnedMessage += ` </ul></div><form action="/message" method="POST">
   <label for="user">username: <input id="user" name="user" type="text"> </input> </label>
   <label for="message">message: <input id="message" name="message" type="text"> </input> </label>
   <label for="sender"><input type="submit"></input></label>
-</form>`);
+  </form>`;
+    res.send(returnedMessage);
+  });
 });
 
 app.listen(port, () => {
